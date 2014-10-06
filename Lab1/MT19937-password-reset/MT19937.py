6861ac7aef596b1879e56a924630219e40ec3c53
Brent Williams <brentwilliams@pcp079530pcs.wireless.calpoly.edu>
2014-10-05 18:05:40 -0700

    Fixed spacing bug

--- Lab1/MT19937-password-reset/MT19937.py
+++ Lab1/MT19937-password-reset/MT19937.py
@@ -1,8 +1,8 @@
 
 #Mersenne Twister MT 19937
 class MT19937:
-	def __init__(self, seed):
-		self.index = 0
+   def __init__(self, seed):
+      self.index = 0
       self.MT = [0] * 624
       
       # Initialize the generator from a seed
@@ -10,8 +10,8 @@ class MT19937:
       for i in range(1,624):
          self.MT[i] = 0xFFFFFFFF & (1812433253 * (self.MT[i-1] ^ (self.MT[i-1] >> 30)) + i) 
 
-	def extract_number(self):
-		# Extract a tempered pseudorandom number based on the index-th value,
+   def extract_number(self):
+      # Extract a tempered pseudorandom number based on the index-th value,
       # calling generate_numbers() every 624 numbers
       if self.index == 0:
          self.generate_numbers()
@@ -25,8 +25,8 @@ class MT19937:
       self.index = (self.index + 1) % 624
       return y
 
-	def generate_number(self):
-		for i in range(1,624):
+   def generate_number(self):
+      for i in range(1,624):
          y = (self.MT[i] & 0x80000000) + (self.MT[(i+1) % 624] & 0x7fffffff)
 
          self.MT[i] = self.MT[(i + 397) % 624] ^ (y >> 1)
