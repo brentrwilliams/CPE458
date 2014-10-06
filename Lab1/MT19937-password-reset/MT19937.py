054f61542c3ef692103921445b3743140c4c0bd0
Brent Williams <brentwilliams@pcp079530pcs.wireless.calpoly.edu>
2014-10-05 17:27:56 -0700

    Updated taskI

--- Lab1/MT19937-password-reset/MT19937.py
+++ Lab1/MT19937-password-reset/MT19937.py
@@ -2,14 +2,35 @@
 #Mersenne Twister MT 19937
 class MT19937:
 	def __init__(self, seed):
-		#TODO: Initialize MT state here
-		return
+		self.index = 0
+      self.MT = [0] * 624
+      
+      # Initialize the generator from a seed
+      self.MT[0] = seed
+      for i in range(1,624):
+         self.MT[i] = 0xFFFFFFFF & (1812433253 * (self.MT[i-1] ^ (self.MT[i-1] >> 30)) + i) 
 
 	def extract_number(self):
-		#TODO: Temper and Extract Here
-		return 42
+		# Extract a tempered pseudorandom number based on the index-th value,
+      # calling generate_numbers() every 624 numbers
+      if self.index == 0:
+         self.generate_numbers()
+
+      y = self.MT[self.index]
+      y = y ^ (y >> 11)
+      y = y ^ ((y << 7) & 2636928640)
+      y = y ^ ((y << 15) & 4022730752)
+      y = y ^ (y >> 18)
+
+      self.index = (self.index + 1) % 624
+      return y
 
 	def generate_number(self):
-		#TODO: Mix state here
-		return
+		for i in range(1,624):
+         y = (self.MT[i] & 0x80000000) + (self.MT[(i+1) % 624] & 0x7fffffff)
+
+         self.MT[i] = self.MT[(i + 397) % 624] ^ (y >> 1)
+
+         if (y % 2) != 0: # y is odd
+            self.MT[i] = self.MT[i] ^ 2567483615
 
--- Lab1/TaskI.py
+++ Lab1/TaskI.py
@@ -3,8 +3,27 @@
 import random
 import time
 from MersenneTwister import MersenneTwister
+from CryptoUtils import intToBase64
 
-def oracle():
+def oracle(randNum):
+   print 'Searching for seed...'
+
+   maxSeed = 2**32
+   twoMinutes = 120
+   startingPoint = int(time.time() - twoMinutes)
+
+   for i in xrange(startingPoint, maxSeed):
+      mt = MersenneTwister(i)
+      retVal = mt.extract_number()
+
+      if retVal == randNum:
+         return intToBase64(i)
+
+   print 'Error: Could not find seed'
+   return ''
+
+
+def main():
    timeToSleep = random.randint(5,60)
    print 'Sleeping for ' + str(timeToSleep) + ' seconds...'
    print ''
@@ -22,29 +41,15 @@ def oracle():
 
    randNum = mt.extract_number()
 
-   print '\tFound Seed:\t' + str(get_seed(randNum))
-   print '\tActual Seed:\t' + str(seed)
+   print '\tFound Seed:\t' + oracle(randNum)
+   print '\tActual Seed:\t' + intToBase64(seed)
 
-
-def get_seed(randNum):
-   print 'Searching for seed...'
-
-   maxSeed = 2**32
-   twoMinutes = 120
-   startingPoint = int(time.time() - twoMinutes)
-
-   for i in xrange(startingPoint, maxSeed):
-      mt = MersenneTwister(i)
-      retVal = mt.extract_number()
-
-      if retVal == randNum:
-         return i
-
-   print 'Error: Could not find seed'
-   return 0
-
-def main():
-   oracle()
 
 if __name__ == '__main__':
    main()
+
+
+
+
+
+
