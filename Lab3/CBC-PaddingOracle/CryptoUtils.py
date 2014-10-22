f2cf8b01941b025dff23008f390042e93f252209
Brent Williams <brent.robert.williams@gmail.com>
2014-10-22 12:36:12 -0700

    Added CryptoUtils to the lab3

---
+++ Lab3/CBC-PaddingOracle/CryptoUtils.py
@@ -0,0 +1,19 @@
+import base64
+
+def asciiToHex(text):
+   return text.encode(encoding='hex', errors='strict')
+
+def hexToAscii(text):
+   return text.decode(encoding='hex', errors='strict')
+
+def asciiToBase64(text):
+   return text.encode(encoding='base64',errors='strict') 
+
+def base64ToAscii(text):
+   return text.decode(encoding='base64',errors='strict')
+
+def base64ToHex(text):
+   return asciiToHex(base64ToAscii(text))
+
+def hexToBase64(text):
+   return asciiToBase64(hexToAscii(text))
