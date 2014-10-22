939244674b60222efbf0d46e93286fafdc4b32a2
Brent Williams <brent.robert.williams@gmail.com>
2014-10-22 12:31:12 -0700

    Added check ciphertext function to ask the server if the padding is correct

--- Lab3/CBC-PaddingOracle/Lab3.py
+++ Lab3/CBC-PaddingOracle/Lab3.py
@@ -13,11 +13,13 @@ def start_server():
    print ''
    return p
 
+
 def stop_server(server):
    print 'Stopping Server...'
    server.terminate()
    time.sleep(1) # make sure it doesn't leave the server as a zombie process
    print 'Server Stopped...'
+
 
 def get_ciphertext():
    resp = urllib2.urlopen('http://0.0.0.0:8080/eavesdrop')
@@ -32,10 +34,31 @@ def get_ciphertext():
 
    return ciphertext
 
+
+def check_ciphertext(ciphertext):
+   result = False
+
+   try:
+      resp = urllib2.urlopen("http://0.0.0.0:8080/?enc=" + ciphertext)
+   except Exception, e:
+      
+      if str(e) == 'HTTP Error 404: Not Found':
+         result = True
+      elif str(e) == 'HTTP Error 403: Forbidden':
+         result = False
+      else:
+         raise e
+
+   return result
+
+
 def main():
    server = start_server()
 
-   print get_ciphertext()
+   ciphertext = get_ciphertext()
+   print ciphertext
+
+   print check_ciphertext(ciphertext)
 
    raw_input() # Wait to stop the server
    stop_server(server)
